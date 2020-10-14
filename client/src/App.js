8b4e8cde7d9c5d5d822e403544dd1fe55eb5b269
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-10-14 21:53:11 +0200

    Add server websockets and UI functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,74 @@
 import React from 'react';
+import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends React.Component {
+  state = {
+    tasks: [
+      {id: 1, name: 'Shoping'},
+      {id: 2, name: 'Walk the dog'}
+    ],
+    taskName: ''
+  }
+  componentDidMount() {
+    this.socket = io.connect('http://localhost:8000');
+    this.socket.on('addTask', task => {
+      this.addTask(task);
+    });
+    this.socket.on('removeTask', deleteTask => {
+      this.removeTask(deleteTask);
+    });
+    this.socket.on('updateData', allTasks => {
+      this.updateTasks(allTasks);
+    });
+  }
+
+  removeTask(task, status) {
+    const { tasks } = this.state;
+    this.setState({
+      tasks: tasks.filter(item => item.id !== task.id)
+    });
+    if(status !== undefined) {
+      this.socket.emit('removeTask', task);
+    }
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    const id = uuidv4();
+    this.addTask({id: id, name: this.state.taskName});
+    this.socket.emit('addTask', ({id: id, name: this.state.taskName}));
+    this.setState({taskName: ''});
+  }
+
+  addTask(task) {
+    this.setState({tasks: [...this.state.tasks, task]});
+  }
+
+  updateTasks(allTasks) {
+    this.setState({tasks: allTasks});
+  }
+ 
   render() {
+    const { tasks, taskName } = this.state;
     return (
-    <div className="App">
-      Hello world
-    </div>
+      <div className="App">
+        <header>
+          <h1>ToDoList.app</h1>
+        </header>
+        <section className="tasks-section" id="tasks-section">
+          <h2>Tasks</h2>
+          <ul className="tasks-section__list" id="tasks-list">
+            {tasks.map(task => (
+              <li key={task.id} className="task">{task.name} <button className="btn btn--red" onClick={() => this.removeTask(task, 'local')}>Remove</button></li>
+            ))}
+          </ul>
+          <form id="add-task-form">
+            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" value={taskName} onChange={(event) => {this.setState({taskName: event.target.value})}}/>
+            <button className="btn" type="submit" onClick={(event) => this.submitForm(event)}>Add</button>
+          </form>
+        </section>
+      </div>
   );
   }
 }
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
-const port = 8000;
+const port =  process.env.PORT || 8000;
 
+let tasks = [];
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+const io = socket(server);
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', task => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', deleteTask => {
+    tasks.filter(task => {
+      return task.id !== deleteTask.id
+    });
+    socket.broadcast.emit('removeTask', deleteTask);
+  });
 });
